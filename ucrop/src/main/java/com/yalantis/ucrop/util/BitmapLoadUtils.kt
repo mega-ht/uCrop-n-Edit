4f9ce66b598e3080864ca549959d1180ce4120f9
Frosch2010 <jens.muen@web.de>
2023-01-28 14:40:18 +0100

    BitmapLoadUtils migrated to kotlin

--- ucrop/src/main/java/com/yalantis/ucrop/util/BitmapLoadUtils.kt
+++ ucrop/src/main/java/com/yalantis/ucrop/util/BitmapLoadUtils.kt
@@ -1,123 +1,101 @@
-package com.yalantis.ucrop.util;
+package com.yalantis.ucrop.util
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Matrix;
-import android.graphics.Point;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.util.Log;
-import android.view.Display;
-import android.view.WindowManager;
-
-import com.yalantis.ucrop.callback.BitmapLoadCallback;
-import com.yalantis.ucrop.task.BitmapLoadTask;
-
-import java.io.Closeable;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.exifinterface.media.ExifInterface;
+import android.content.Context
+import android.graphics.*
+import android.net.Uri
+import android.os.AsyncTask
+import android.util.Log
+import android.view.Display
+import android.view.WindowManager
+import androidx.exifinterface.media.ExifInterface
+import com.yalantis.ucrop.callback.BitmapLoadCallback
+import com.yalantis.ucrop.task.BitmapLoadTask
+import com.yalantis.ucrop.util.EglUtils.maxTextureSize
+import java.io.Closeable
+import java.io.IOException
 
 /**
  * Created by Oleksii Shliama (https://github.com/shliama).
  */
-public class BitmapLoadUtils {
-
-    private static final String TAG = "BitmapLoadUtils";
-
-    public static void decodeBitmapInBackground(@NonNull Context context,
-                                                @NonNull Uri uri, @Nullable Uri outputUri,
-                                                int requiredWidth, int requiredHeight,
-                                                BitmapLoadCallback loadCallback) {
-
-        new BitmapLoadTask(context, uri, outputUri, requiredWidth, requiredHeight, loadCallback)
-                .executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+object BitmapLoadUtils {
+    private const val TAG = "BitmapLoadUtils"
+    @JvmStatic
+    fun decodeBitmapInBackground(
+        context: Context,
+        uri: Uri, outputUri: Uri?,
+        requiredWidth: Int, requiredHeight: Int,
+        loadCallback: BitmapLoadCallback?
+    ) {
+        BitmapLoadTask(context, uri, outputUri, requiredWidth, requiredHeight, loadCallback!!)
+            .executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
     }
 
-    public static Bitmap transformBitmap(@NonNull Bitmap bitmap, @NonNull Matrix transformMatrix) {
+    fun transformBitmap(bitmap: Bitmap, transformMatrix: Matrix): Bitmap {
+        var bitmap = bitmap
         try {
-            Bitmap converted = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), transformMatrix, true);
+            val converted = Bitmap.createBitmap(
+                bitmap,
+                0,
+                0,
+                bitmap.width,
+                bitmap.height,
+                transformMatrix,
+                true
+            )
             if (!bitmap.sameAs(converted)) {
-                bitmap = converted;
+                bitmap = converted
             }
-        } catch (OutOfMemoryError error) {
-            Log.e(TAG, "transformBitmap: ", error);
+        } catch (error: OutOfMemoryError) {
+            Log.e(TAG, "transformBitmap: ", error)
         }
-        return bitmap;
+        return bitmap
     }
 
-    public static int calculateInSampleSize(@NonNull BitmapFactory.Options options, int reqWidth, int reqHeight) {
+    fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
         // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
+        val height = options.outHeight
+        val width = options.outWidth
+        var inSampleSize = 1
         if (height > reqHeight || width > reqWidth) {
             // Calculate the largest inSampleSize value that is a power of 2 and keeps both
             // height and width lower or equal to the requested height and width.
-            while ((height / inSampleSize) > reqHeight || (width / inSampleSize) > reqWidth) {
-                inSampleSize *= 2;
+            while (height / inSampleSize > reqHeight || width / inSampleSize > reqWidth) {
+                inSampleSize *= 2
             }
         }
-        return inSampleSize;
+        return inSampleSize
     }
 
-    public static int getExifOrientation(@NonNull Context context, @NonNull Uri imageUri) {
-        int orientation = ExifInterface.ORIENTATION_UNDEFINED;
+    fun getExifOrientation(context: Context, imageUri: Uri): Int {
+        var orientation = ExifInterface.ORIENTATION_UNDEFINED
         try {
-            InputStream stream = context.getContentResolver().openInputStream(imageUri);
-            if (stream == null) {
-                return orientation;
-            }
-            orientation = new ImageHeaderParser(stream).getOrientation();
-            close(stream);
-        } catch (IOException e) {
-            Log.e(TAG, "getExifOrientation: " + imageUri.toString(), e);
+            val stream = context.contentResolver.openInputStream(imageUri) ?: return orientation
+            orientation = ImageHeaderParser(stream).orientation
+            close(stream)
+        } catch (e: IOException) {
+            Log.e(TAG, "getExifOrientation: $imageUri", e)
         }
-        return orientation;
+        return orientation
     }
 
-    public static int exifToDegrees(int exifOrientation) {
-        int rotation;
-        switch (exifOrientation) {
-            case ExifInterface.ORIENTATION_ROTATE_90:
-            case ExifInterface.ORIENTATION_TRANSPOSE:
-                rotation = 90;
-                break;
-            case ExifInterface.ORIENTATION_ROTATE_180:
-            case ExifInterface.ORIENTATION_FLIP_VERTICAL:
-                rotation = 180;
-                break;
-            case ExifInterface.ORIENTATION_ROTATE_270:
-            case ExifInterface.ORIENTATION_TRANSVERSE:
-                rotation = 270;
-                break;
-            default:
-                rotation = 0;
+    fun exifToDegrees(exifOrientation: Int): Int {
+        val rotation: Int
+        rotation = when (exifOrientation) {
+            ExifInterface.ORIENTATION_ROTATE_90, ExifInterface.ORIENTATION_TRANSPOSE -> 90
+            ExifInterface.ORIENTATION_ROTATE_180, ExifInterface.ORIENTATION_FLIP_VERTICAL -> 180
+            ExifInterface.ORIENTATION_ROTATE_270, ExifInterface.ORIENTATION_TRANSVERSE -> 270
+            else -> 0
         }
-        return rotation;
+        return rotation
     }
 
-    public static int exifToTranslation(int exifOrientation) {
-        int translation;
-        switch (exifOrientation) {
-            case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
-            case ExifInterface.ORIENTATION_FLIP_VERTICAL:
-            case ExifInterface.ORIENTATION_TRANSPOSE:
-            case ExifInterface.ORIENTATION_TRANSVERSE:
-                translation = -1;
-                break;
-            default:
-                translation = 1;
+    fun exifToTranslation(exifOrientation: Int): Int {
+        val translation: Int
+        translation = when (exifOrientation) {
+            ExifInterface.ORIENTATION_FLIP_HORIZONTAL, ExifInterface.ORIENTATION_FLIP_VERTICAL, ExifInterface.ORIENTATION_TRANSPOSE, ExifInterface.ORIENTATION_TRANSVERSE -> -1
+            else -> 1
         }
-        return translation;
+        return translation
     }
 
     /**
@@ -127,50 +105,47 @@ public class BitmapLoadUtils {
      *
      * @return - max bitmap size in pixels.
      */
-    @SuppressWarnings({"SuspiciousNameCombination", "deprecation"})
-    public static int calculateMaxBitmapSize(@NonNull Context context) {
-        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        Display display;
-        int width, height;
-        Point size = new Point();
-
+    @JvmStatic
+    fun calculateMaxBitmapSize(context: Context): Int {
+        val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display: Display
+        val width: Int
+        val height: Int
+        val size = Point()
         if (wm != null) {
-            display = wm.getDefaultDisplay();
-            display.getSize(size);
+            display = wm.defaultDisplay
+            display.getSize(size)
         }
-
-        width = size.x;
-        height = size.y;
+        width = size.x
+        height = size.y
 
         // Twice the device screen diagonal as default
-        int maxBitmapSize = (int) Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+        var maxBitmapSize =
+            Math.sqrt(Math.pow(width.toDouble(), 2.0) + Math.pow(height.toDouble(), 2.0)).toInt()
 
         // Check for max texture size via Canvas
-        Canvas canvas = new Canvas();
-        final int maxCanvasSize = Math.min(canvas.getMaximumBitmapWidth(), canvas.getMaximumBitmapHeight());
+        val canvas = Canvas()
+        val maxCanvasSize = Math.min(canvas.maximumBitmapWidth, canvas.maximumBitmapHeight)
         if (maxCanvasSize > 0) {
-            maxBitmapSize = Math.min(maxBitmapSize, maxCanvasSize);
+            maxBitmapSize = Math.min(maxBitmapSize, maxCanvasSize)
         }
 
         // Check for max texture size via GL
-        final int maxTextureSize = EglUtils.getMaxTextureSize();
+        val maxTextureSize = maxTextureSize
         if (maxTextureSize > 0) {
-            maxBitmapSize = Math.min(maxBitmapSize, maxTextureSize);
+            maxBitmapSize = Math.min(maxBitmapSize, maxTextureSize)
         }
-
-        Log.d(TAG, "maxBitmapSize: " + maxBitmapSize);
-        return maxBitmapSize;
+        Log.d(TAG, "maxBitmapSize: $maxBitmapSize")
+        return maxBitmapSize
     }
 
-    @SuppressWarnings("ConstantConditions")
-    public static void close(@Nullable Closeable c) {
-        if (c != null && c instanceof Closeable) { // java.lang.IncompatibleClassChangeError: interface not implemented
+    fun close(c: Closeable?) {
+        if (c != null && c is Closeable) { // java.lang.IncompatibleClassChangeError: interface not implemented
             try {
-                c.close();
-            } catch (IOException e) {
+                c.close()
+            } catch (e: IOException) {
                 // silence
             }
         }
     }
-
 }
