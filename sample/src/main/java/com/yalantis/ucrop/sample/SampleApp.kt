3fa83ea7f0f1546e974710dc9396ed276483e321
Frosch2010 <jens.muen@web.de>
2023-02-10 11:09:44 +0100

    translated SampleApp.java to kotlin

--- sample/src/main/java/com/yalantis/ucrop/sample/SampleApp.kt
+++ sample/src/main/java/com/yalantis/ucrop/sample/SampleApp.kt
@@ -1,32 +1,24 @@
-package com.yalantis.ucrop.sample;
+package com.yalantis.ucrop.sample
 
-import android.app.Application;
+import android.app.Application
+import com.yalantis.ucrop.UCropHttpClientStore
+import okhttp3.ConnectionSpec
+import okhttp3.OkHttpClient
 
-import com.yalantis.ucrop.UCropHttpClientStore;
-
-import java.util.Collections;
-
-import okhttp3.ConnectionSpec;
-import okhttp3.OkHttpClient;
-
-public class SampleApp extends Application {
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        setUcropHttpClient();
+class SampleApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        setUcropHttpClient()
     }
 
-    private void setUcropHttpClient() {
-        ConnectionSpec cs = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+    private fun setUcropHttpClient() {
+        val cs: ConnectionSpec = ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
             .allEnabledCipherSuites()
             .allEnabledTlsVersions()
-            .build();
-
-        OkHttpClient client = new OkHttpClient.Builder()
-            .connectionSpecs(Collections.singletonList(cs))
-            .build();
-
-        UCropHttpClientStore.INSTANCE.setClient(client);
+            .build()
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .connectionSpecs(listOf(cs))
+            .build()
+        UCropHttpClientStore.INSTANCE.client = client
     }
 }
